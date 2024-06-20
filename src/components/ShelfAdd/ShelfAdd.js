d73b6e3a13ead956834c5d1e1e2535686ff06aca
Sumeiya Abdi <sumeiya619@gmail.com>
2024-06-20 15:52:10 -0500

    changed location

--- src/components/ShelfAdd/ShelfAdd.js
+++ src/components/ShelfAdd/ShelfAdd.js
@@ -2,18 +2,47 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function ShelfAdd() {
-  
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    // Dispatch action with payload
+    dispatch({ type: 'ADD_ITEM_SUCCESS', payload: { description, image } });
+
+    // Clear input fields
+    setDescription('');
+    setImage('');
   }
 
   return (
-    
+    <>
       <div className="container">
         <h2>Shelf</h2>
         <p>All of the available items can be seen here.</p>
       </div>
 
+      <form onSubmit={handleSubmit}> 
+        <input 
+          type='text'
+          placeholder='Description'
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
 
+        <input 
+          type='text'
+          placeholder='Image URL'
+          value={image}
+          onChange={(event) => setImage(event.target.value)}
+        />
+
+        <button type='submit'>Submit</button>
+      </form>
+    </>
   );
-
+}
 
 export default ShelfAdd;
