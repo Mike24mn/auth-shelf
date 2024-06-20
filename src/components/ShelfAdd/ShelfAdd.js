9389520eac150ae7b91075617b8544566062386a
Sumeiya Abdi <sumeiya619@gmail.com>
2024-06-20 15:37:03 -0500

    switched shelf page

---
+++ src/components/ShelfAdd/ShelfAdd.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function ShelfAdd() {
+  
+  }
+
+  return (
+    
+      <div className="container">
+        <h2>Shelf</h2>
+        <p>All of the available items can be seen here.</p>
+      </div>
+
+
+  );
+
+
+export default ShelfAdd;
--- src/redux/sagas/addItem.saga.js
+++ src/redux/sagas/addItem.saga.js
@@ -3,18 +3,16 @@ import axios from 'axios';
 
 function* addItem(action) {
     try {
-       
-const response = yield axios.post('/api/shelf', action.payload) // action.payload may need to be destructured with {} around action.payload
-yield put({type: `ADD_ITEM_SUCCESS`, payload: response.data })
+        const response = yield axios.post('/api/shelf', action.payload);
+        yield put({ type: 'ADD_ITEM_SUCCESS', payload: response.data });
     } catch (error) {
-        console.log('error with shelf POST request', error);
-        yield put ({ type: `ADD_ITEM_FAILED`});
+        console.error('Error with shelf POST request', error);
+        yield put({ type: 'ADD_ITEM_FAILED' });
     }
 }
 
 function* addItemSaga() {
-    yield takeLatest('ADD_ITEM_SUCCESS', addItem)
+    yield takeLatest('ADD_ITEM', addItem); 
 }
 
-
-export default addItem
+export default addItemSaga;
