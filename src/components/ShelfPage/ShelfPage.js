d303da682432d1e54c4106691a1ba42e21e3c6f2
Sumeiya Abdi <sumeiya619@gmail.com>
2024-06-20 15:16:55 -0500

    work in progress

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ShelfPage() {
-  return (
+
+const [description, setDescription] = useState('')
+const [image, setImage] = useState('')
+  const addItem = useSelector(store => store.addItem)
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    dispatch({type: 'ADD_ITEM_SUCCESS', payload: description, image })
+    setDescription('');
+    setImage('');
+  }
+
+  return (<>
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
     </div>
-  );
+
+    <input 
+    type='text'
+    placeholder='Description'
+    value={description}
+    onChange={(event) => setDescription(event.target.value)}
+    />
+
+<input 
+    type='text'
+    placeholder='Image URL'
+    value={image}
+    onChange={(event) => setImage(event.target.value)}
+    />
+    </> );
 }
 
 export default ShelfPage;
