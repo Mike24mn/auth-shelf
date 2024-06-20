af2ab5d48b02ec665cf98af00f5efa4929fda3e0
Sumeiya Abdi <sumeiya619@gmail.com>
2024-06-20 15:54:34 -0500

    original

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
 function ShelfPage() {
-
-
-
-
-
   return (
- 
-      <div className="container">
-        <h2>Shelf</h2>
-        <p>All of the available items can be seen here.</p>
-      </div>
-
-   
+    <div className="container">
+      <h2>Shelf</h2>
+      <p>All of the available items can be seen here.</p>
+    </div>
   );
 }
 
